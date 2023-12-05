93ea3ccce3559aa3fe7bc022d2db5a58f3aef40c
Neutroflash <neutroflashp@hotmail.com>
2023-12-05 17:09:10 -0500

    Add styles and useState for switch

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,24 +1,35 @@
 import * as React from "react";
 import data from "../../../data.json";
 import styles from "./SideBar.module.css";
+import * as IoIcons from "react-icons/io";
+import { RxSwitch } from "react-icons/rx";
+
+const style = { color: "white", fontSize: "1.2em" }
 
 const SideBar = () => {
   const [initial, setInitial] = React.useState(true);
+  
+  const toggleSideBar = () => {
+    console.log("click en el boton")
+    setInitial(!initial)
+  }
 
   return (
-    <div className={styles.sideBar}>
-      <ul>
-        {data.map((item, index) => (
-          <li key={item.id} className={styles.sideBar_item}>
-            {index === 0 ? (
-              <img src={item.image} alt={item.id} className={styles.myPhoto} />
-            ) : (
-              <img src={item.image} alt={item.id} />
-            )}
-            <p>{item.text}</p>
-          </li>
-        ))}
-      </ul>
+    <div className={`${styles.sideBar} ${initial ? styles.open : ""}`}>
+      {initial &&(
+        <ul>
+          {data.map((item) => (
+            <li key={item.id} className={styles.sideBar_item}>
+              {item.id === 0 ? (
+                <img src={item.image} alt={item.id} className={styles.myPhoto} />
+              ) : (
+                item.id === 7 ? <button className={styles.sideBar_switch} onClick={toggleSideBar}><RxSwitch/></button> : React.createElement(IoIcons[item.icon],{ style: style,  className: styles.sideBar_link})
+              )}
+              <p>{item.text}</p>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
