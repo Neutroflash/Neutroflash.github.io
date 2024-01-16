4d2cb31565f149db205959aeb67eb3498c46c8ab
Neutroflash <neutroflashp@hotmail.com>
2024-01-15 21:08:17 -0500

    Fixed something and add more components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,12 @@
 import * as React from "react"
+import styles from "./About.module.css"
 
 const About = () => {
     return (
-        <p>About</p>
+        <div className={styles.about}>
+            <h1>Acerca de mi</h1>
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis maxime totam. Nobis vero hic ipsa maiores asperiores, doloremque blanditiis repudiandae nesciunt mollitia, sapiente quas. Ab, quaerat nulla. Corporis, voluptatum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis maxime totam. Nobis vero hic ipsa maiores asperiores, doloremque blanditiis repudiandae nesciunt mollitia, sapiente quas. Ab, quaerat nulla. Corporis, voluptatum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis maxime totam. Nobis vero hic ipsa maiores asperiores, doloremque blanditiis repudiandae nesciunt mollitia, sapiente quas. Ab, quaerat nulla. Corporis, voluptatum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis maxime totam. Nobis vero hic ipsa maiores asperiores, doloremque blanditiis repudiandae nesciunt mollitia, sapiente quas. Ab, quaerat nulla. Corporis, voluptatum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis maxime totam. Nobis vero hic ipsa maiores asperiores, doloremque blanditiis repudiandae nesciunt mollitia, sapiente quas. Ab, quaerat nulla. Corporis, voluptatum?</h3>
+        </div>
     )
 }
 
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styles from "./Skills.module.css"
+
+const Skills = () => {
+    return (
+        <div className={styles.skills}>
+            <h1>Conocimientos</h1>
+        </div>
+    )
+}
+
+export default Skills
