e51834f1a561486ffcb9a76174391f20fc2c0c85
Neutroflash <neutroflashp@hotmail.com>
2024-01-23 09:08:36 -0500

    Add v1

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,4 @@
 import * as React from "react"
-
-import image2 from "../../assets/photo1.jpeg"
-import image1 from  "../../assets/photo2.jpeg"
-import image3 from "../../assets/memories.jpeg"
-
 import styles from "./About.module.css"
 
 const About = () => {
@@ -15,11 +10,6 @@ const About = () => {
                 <p>Mi trayectoria académica se enfocó en la ingeniería de software durante mis años universitarios, donde desarrollé aplicaciones de consola en C++. Sin embargo, mi verdadero redescubrimiento ocurrió al sumergirme en el fascinante mundo del desarrollo web. Mi enfoque se centró en el front end, donde disfrutaba plasmar diseños en código, fusionando creatividad y funcionalidad.</p>
                 <p>El siguiente capítulo en mi camino fue la participación en Codeable, un bootcamp de programación Full Stack. Esta experiencia intensiva me permitió explorar nuevos campos y adquirir habilidades que nunca había imaginado. Como egresado de Codeable, me convertí en un Full Stack developer, potenciando y consolidando todos mis conocimientos.</p>
                 <p>En mí encontrarás no solo habilidades técnicas, sino también una inquebrantable voluntad de aprender y mejorar constantemente. Mi motivación se refleja en cada línea de código que escribo y en cada proyecto en el que me embarco. Estoy emocionado por la oportunidad de unirme a tu equipo, contribuir con mi experiencia y seguir creciendo juntos en este apasionante mundo tecnológico. ¡Hagamos cosas increíbles juntos!</p>
-            </div>
-            <div className={styles.aboutImages}>
-                <img className={styles.image1} src={image1} alt="image1"/>
-                <img className={styles.image1} src={image2} alt="image2"/>
-                <img className={styles.image2} src={image3} alt="image3"/>
             </div>
         </div>
     )
